a26a5cf14c697818192d1a7d5ebfe1bf97712156
blackJJW <jjinwoo92@gmail.com>
2022-07-04 09:42:29 +0900

    2022-07-04

---
+++ Test/src/test/java/com/test/persistence/JDBCTest.java
@@ -0,0 +1,40 @@
+package com.test.persistence;
+
+import static org.junit.Assert.fail;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+
+public class JDBCTest {
+	static { 
+        try { 
+            Class.forName("oracle.jdbc.driver.OracleDriver"); 
+        } catch(Exception e) { 
+            e.printStackTrace(); 
+        } 
+    }
+	
+    
+    @Test 
+    public void testConnection() { 
+    	//given, when, then
+    	//given(필요한 데이터 준비)
+    	String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+    	String id = "BlackJJW";
+    	String password = "2155"; 
+    	
+    	//when실행
+        try(Connection con = DriverManager.getConnection( 
+        		url, 
+        		id, 
+        		password)){ 
+        	
+        //then
+            System.out.println(con); 
+        } catch (Exception e) { 
+            fail(e.getMessage()); 
+        } 
+    
+    }    
+}
