4cf9ddb512bd37106e4a9ddda58c93554564a523
blackJJW <jjinwoo92@gmail.com>
2022-07-05 10:36:27 +0900

    등록 완료 메시지 alert 완료

--- Test/src/main/java/com/test/controller/BoardController.java
+++ Test/src/main/java/com/test/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.test.model.BoardVO;
 import com.test.service.BoardService;
@@ -43,11 +44,13 @@ public class BoardController {
     
     /* 게시판 등록처리 */
     @PostMapping("/enroll")
-    public String boardEnrollPOST(BoardVO board) {
+    public String boardEnrollPOST(BoardVO board, RedirectAttributes rttr) {
         
         log.info("BoardVO : " + board);
         
         bservice.enroll(board);
+        
+        rttr.addFlashAttribute("result", "enroll success");
         
         return "redirect:/board/list";
         
