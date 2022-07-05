b3c0ab855a5d2aafd33da5cf1d3317664a9b0f1e
blackJJW <jjinwoo92@gmail.com>
2022-07-05 10:17:44 +0900

    board list, enroll, enroll(post)

--- Test/src/main/java/com/test/mapper/BoardMapper.java
+++ Test/src/main/java/com/test/mapper/BoardMapper.java
@@ -1,8 +1,13 @@
 package com.test.mapper;
 
+import java.util.List;
+
 import com.test.model.BoardVO;
 
 public interface BoardMapper {
 	/* 글 등록 */
     public void enroll(BoardVO board);
+    
+    /* 게시판 목록 */
+    public List<BoardVO> getList();
 }
--- Test/src/test/java/com/test/mapper/BoardMapperTests.java
+++ Test/src/test/java/com/test/mapper/BoardMapperTests.java
@@ -1,5 +1,9 @@
 package com.test.mapper;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -14,21 +18,40 @@ import com.test.model.BoardVO;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 public class BoardMapperTests {
 	private static final Logger log = LoggerFactory.getLogger(BoardMapperTests.class);
-    
-    @Autowired
-    private BoardMapper mapper;
 
-    @Test
-    public void testEnroll() {
-        
-        BoardVO vo = new BoardVO();
-        
-        vo.setTitle("mapper test");
-        vo.setContent("mapper test");
-        vo.setWriter("mapper test");
-        
-        mapper.enroll(vo);
-        
-    }
+	@Autowired
+	private BoardMapper mapper;
+
+	@Test
+	public void testEnroll() {
+
+		BoardVO vo = new BoardVO();
+
+		vo.setTitle("mapper test");
+		vo.setContent("mapper test");
+		vo.setWriter("mapper test");
+
+		mapper.enroll(vo);
+
+	}
+
+	/* 게시판 목록 테스트 */
+	@Test
+	public void testGetList() {
+
+		List list = mapper.getList();
+		/*
+		 * 일반적 for문 for(int i = 0; i < list.size();i++) { log.info("" + list.get(i)); }
+		 */
+
+		/* foreach문(향상된 for문) */
+		for (Object a : list) {
+			log.info("" + a);
+		}
+
+		/*
+		 * foreach문 & 람다식 list.forEach(board -> log.info("" + board));
+		 */
+	}
 
 }
