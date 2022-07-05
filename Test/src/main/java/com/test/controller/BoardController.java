b2a2e584d2dfb8c6bc5517baf6714dc75bcd2de9
blackJJW <jjinwoo92@gmail.com>
2022-07-05 10:28:39 +0900

    글쓴 이후 다시 리스트로 가기 완료

--- Test/src/main/java/com/test/controller/BoardController.java
+++ Test/src/main/java/com/test/controller/BoardController.java
@@ -2,18 +2,23 @@ package com.test.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.test.model.BoardVO;
+import com.test.service.BoardService;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
 	
 	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
+	
+	@Autowired
+    private BoardService bservice;
 	
 	@GetMapping("/list")
     // => @RequestMapping(value="list", method=RequestMethod.GET)
@@ -38,9 +43,13 @@ public class BoardController {
     
     /* 게시판 등록처리 */
     @PostMapping("/enroll")
-    public void boardEnrollPOST(BoardVO board) {
+    public String boardEnrollPOST(BoardVO board) {
         
         log.info("BoardVO : " + board);
+        
+        bservice.enroll(board);
+        
+        return "redirect:/board/list";
         
     }
 }
